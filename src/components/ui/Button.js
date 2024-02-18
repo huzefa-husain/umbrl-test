997a08acd999f848164e6528d251ca981ced10d8
huzefa husain <huzefahusain@huzefas-mbp.home>
2024-02-18 10:08:58 +0000

    created ui component for button

---
+++ src/components/ui/Button.js
@@ -0,0 +1,22 @@
+import cx from "classnames";
+const Button = (props) => {
+  const { color = "white", className, disabled, ...rest } = props;
+
+  return (
+    <button
+      className={cx(
+        "rounded-lg font-medium text-[17px] px-8 py-2",
+        {
+          "pointer-events-none opacity-50": disabled,
+          "bg-[#473BF0] text-white": color === "purple",
+          "border border-black bg-white text-black": color === "white",
+        },
+        className
+      )}
+      disabled={disabled}
+      {...rest}
+    />
+  );
+};
+
+export default Button;
